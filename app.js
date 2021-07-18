361e3d0b8d6b3a8fba7e498533d8e57b98e1bb42
Ljime <Ljime@users.noreply.github.com>
2021-07-18 00:38:56 -0400

    production

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(express.json())
 app.use(storyRouter)
 app.use(userRouter)
 
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'))
+    app.get('*' , (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 const port = process.env.PORT || 5000
 app.listen(port)
 
